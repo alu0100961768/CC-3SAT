0c42ce30de38059f6b8298e25c874b55ee307a33
alu0100961768 <32639399+alu0100961768@users.noreply.github.com>
2019-10-10 10:55:54 +0100

    Add files via upload

---
+++ include/SAT3.h
@@ -0,0 +1,20 @@
+/**
+ * Autores: David de León Rodríguez y Victoria Quintana Martí
+ */
+#ifndef SAT3_H
+#define SAT3_H
+
+#include "SAT.h"
+
+using namespace std;
+
+class SAT3 : public SAT {
+  public:
+    SAT3() {};
+
+    void pushClausula(vector<int> clausula);
+
+    friend ifstream &operator>>( ifstream  &fich, SAT3 &S );
+};
+
+#endif
